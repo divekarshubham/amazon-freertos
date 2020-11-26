5d00dfe8d290cbe2f6e42fb2312010cb1ef671e0
Joshua Yan <52796499+yanjos-dev@users.noreply.github.com>
2020-11-25 16:51:48 -0800

    Remove FREERTOS_ENABLE_UNIT_TESTS macro

--- vendors/cypress/MTB/ota/ports/CY8CKIT_064S0S2_4343W/aws_ota_pal.c
+++ vendors/cypress/MTB/ota/ports/CY8CKIT_064S0S2_4343W/aws_ota_pal.c
@@ -1176,8 +1176,3 @@ OtaPalImageState_t prvPAL_GetPlatformImageState( OtaFileContext_t * const C )
 }
 
 /*-----------------------------------------------------------*/
-
-/* Provide access to private members for testing. */
-#ifdef FREERTOS_ENABLE_UNIT_TESTS
-    #include "aws_ota_pal_test_access_define.h"
-#endif
